0bcd408234ef39e5ac2be102833a106f82b03458
dungvvph34412 <vuvandung4k2@gmail.com>
2025-02-19 14:32:36 +0700

    update app/http/Middleware/CheckAdminMiddleware.php

---
+++ app/Http/Middleware/CheckAdminMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class CheckAdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+       if(Auth::check()){ // phai dang nhap
+        if(Auth::user()->role == '1'){
+        return $next($request);
+        }
+       }
+       return redirect()->route('login')->with([ //dang nhap that phai => return: login
+        'messageError' => 'Bạn chưa đăng nhập'
+    ]);
+    }
+}

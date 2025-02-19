9671bcda6d0d958c5ef48680656a2409ec9348f9
dungvvph34412 <vuvandung4k2@gmail.com>
2025-02-19 20:11:20 +0700

    update Controllers/Admin/UserController

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,11 +3,43 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Hash;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function listUsers(){
-        return view('admin.users.list-user');
+       $listUser =  User::select('id', 'name', 'email', 'role')->get();
+        return view('admin.users.list-user')->with([
+            'listUser' => $listUser
+        ]);
+    }
+    
+    public function addUsers(Request $req){
+        $req->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'role' => 'required',
+        ],[
+            'name.required' => 'Name không được để trống',
+            'email.required' => 'Email không được để trống',
+            'password.required' => 'Password không được để trống', 
+            'role.required' => 'Chưa chọn role',
+           ]
+     );
+
+     $newUser = new User();
+     $newUser->name = $req->name;
+     $newUser->email = $req->email;
+     $newUser->password = Hash::make($req->password);
+     $newUser->role = $req->role;
+     $newUser->save();
+     return redirect()->back()->with([
+        'message' => 'thêm mới thành công'
+     ]);
+
     }
 }

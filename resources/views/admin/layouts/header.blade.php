36b4f6f892ee1eece0d3abe0496a449398cb904b
dungvvph34412 <vuvandung4k2@gmail.com>
2025-02-19 14:21:48 +0700

    update views/layouts

---
+++ resources/views/admin/layouts/header.blade.php
@@ -0,0 +1,8 @@
+<div class="bg-primary ps-4 pt-4" style="height: 100px; width: 100%;">
+    <h3 class="text-light">Trang quản trị</h3>
+    <form action="{{ route('logout') }}"  style="display:inline;">
+        <button type="submit" class="btn btn-danger">Đăng xuất</button>
+    </form>
+</div>
+
+
